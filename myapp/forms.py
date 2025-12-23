e63b46e1db02fdb091d99c4cae21b1e55cdbe4d8
joshua <condorenajoshua98@gmail.com>
2025-12-23 17:28:01 -0500

    se implementaron los formularios

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,3 +3,7 @@ from django import forms
 class CreateFormsUser(forms.Form):
     username=forms.CharField()
     profile=forms.CharField()
+
+class CreateFormPr(forms.Form):
+    user=forms.CharField()
+    img=forms.CharField()    
